fa3b68d55934d53c238766d82d3f2968201227cf
Engmohamed-ali05 <bad100899@gmail.com>
2026-08-29 18:27:14 +0300

    1st answer

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace assigment
+﻿
+//G - NET - 101 - C#Basics-02
+//1st answer
+namespace assigment
 
 
 public class book { public string title; public int page; }
@@ -15,30 +18,4 @@ internal class Program
         object ob1 = book;
         Console.WriteLine(ob1);
 
-        Console.WriteLine(book.ToString());
-        Console.WriteLine(book.Equals(book));
-        Console.WriteLine(book.GetHashCode());
-        Console.WriteLine(book.gettype());
-
-        int page = 464;
-        Console.WriteLine(page);
-
-        try
-        {
-            int result = 10 / 0;
-            Console.WriteLine(result);
-        }
-        catch (Divide Exception)
-        {
-            Console.WriteLine("can notdivide");
-        }
-        finally
-        {
-            Console.WriteLine("done");
-        }
-
-        int pageint = 300;
-        double pagedouble = pageint;
-        Console.WriteLine(pagedouble);
-    }
-}
+        
